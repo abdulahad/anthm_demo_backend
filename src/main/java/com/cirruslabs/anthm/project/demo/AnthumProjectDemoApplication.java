dc0b40c065802976fb3c32852e2a2f8ea80156e0
Ahad Jeyemby <abdulahad.jmb@gmail.com>
2021-11-24 09:43:27 -0500

    Initailize Project

---
+++ src/main/java/com/cirruslabs/anthm/project/demo/AnthumProjectDemoApplication.java
@@ -0,0 +1,13 @@
+package com.cirruslabs.anthm.project.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnthumProjectDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnthumProjectDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cirruslabs/anthm/project/demo/AnthumProjectDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cirruslabs.anthm.project.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnthumProjectDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
